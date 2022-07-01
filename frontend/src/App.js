9fa37169ec18c71e9f385a9ccbf9eb6ff5856bc8
Pavi99sandamini151 <84126210+Pavi99sandamini151@users.noreply.github.com>
2022-07-01 12:19:36 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+ 
+function App() {
+  return (
+    <Router>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Switch>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <Route path="/add">
+              <AddProduct />
+            </Route>
+            <Route path="/edit/:id">
+              <EditProduct />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+}
+ 
+export default App;
